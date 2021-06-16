5525d4fdb70766e3704df5ce0703dd580f9df510
YSFJK <joufuro@gmail.com>
2021-06-16 16:48:34 +0900

    4st

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ const index_page=fs.readFileSync('./index.ejs','utf8');
 const other_page=fs.readFileSync('./other.ejs','utf8');
 const style_css=fs.readFileSync('./style.css','utf8');
 
-const server=http.createServer(getFromClient);
-
-
-listen(process.env.PORT, process.env.IP);
+http.createServer(getFromClient).listen(process.env.PORT, process.env.IP);
 console.log('SERVER OPEN');
 
 function getFromClient(request,response){
